4a464a306b8f4554ab7cbc9f5349b376bd69b80e
maimaail <ailiniyazi.maimaiti@diconium.com>
2023-01-31 09:19:05 +0100

    Initial Commit

---
+++ Salwador/SalwadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalwadorApp.swift
+//  Salwador
+//
+//  Created by Ailiniyazi Maimaiti on 31.01.23.
+//
+
+import SwiftUI
+
+@main
+struct SalwadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
