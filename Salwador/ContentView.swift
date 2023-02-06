4a471d4d58cac950b172be9c0b1599325ff96585
maimaail <54360982+memeteli@users.noreply.github.com>
2023-02-06 13:12:38 +0100

    feat: add image, text and button element

--- Salwador/ContentView.swift
+++ Salwador/ContentView.swift
@@ -7,16 +7,37 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+              ZStack{
+                        Color(.black)
+                   
+                                  .ignoresSafeArea()
+                        VStack {
+                                  Button("Tap on me") {
+                                
+                                            print("hello world")
+                                  }
+                                  Image("salvador-man").resizable()
+                                            .cornerRadius(15)
+                                            .aspectRatio(contentMode: .fit)
+                                  Text("Salvador iOS App")
+                                            .font(.title)
+                                            .fontWeight(.bold)
+                                  
+                        }
+              }
+
+        
         .padding()
     }
+}
+
+func validate () {
+          print("text fiedld");
 }
 
 struct ContentView_Previews: PreviewProvider {
