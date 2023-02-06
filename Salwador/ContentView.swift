bd5265e06d1358390eeb6952a651d89d6297a440
maimaail <54360982+memeteli@users.noreply.github.com>
2023-02-06 14:28:58 +0100

    added views

--- Salwador/ContentView.swift
+++ Salwador/ContentView.swift
@@ -11,37 +11,86 @@ import SwiftUI
 
 struct ContentView: View {
 
-    var body: some View {
-              ZStack{
-                        Color(.black)
-                   
-                                  .ignoresSafeArea()
-                        VStack {
-                                  Button("Tap on me") {
-                                
-                                            print("hello world")
-                                  }
-                                  Image("salvador-man").resizable()
-                                            .cornerRadius(15)
-                                            .aspectRatio(contentMode: .fit)
-                                  Text("Salvador iOS App")
-                                            .font(.title)
-                                            .fontWeight(.bold)
-                                  
-                        }
-              }
+          @State private var text: String = ""
+          @State private var savedText: String = ""
+          @State private var descriptionText: String = "Your promp will be shown here"
 
-        
+          var body: some View {
+              ZStack{
+                        Color(red: 0.0, green: 0.4666666666666667, blue: 0.7137254901960784)
+                                                      .ignoresSafeArea()
+    VStack() {
+              appNameView
+           editorViewDescription
+              textEditorView
+              submitButton
+              submittedTextDescription
+              submittedText
+    }
+                    
+              }
         .padding()
     }
 }
 
-func validate () {
-          print("text fiedld");
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+private extension ContentView {
+          var appNameView: some View {
+                    Text("Salvador")
+                              .font(.title)
+                              .fontWeight(.bold)
+                              .padding(.bottom, 30)
+                    
+          }
+          var textEditorView: some View {
+                    TextEditor(text: $text)
+                              .background(Color(.red))
+                              .font(.title2)
+                              .frame(height: 250)
+                              .border(Color(.black), width: 1)
+                              .padding(10)
+                                             .onTapGesture {}
+          }
+          
+          var editorViewDescription: some View {
+                    Text("Please enter your promp in the editor")
+                              .font(.title2)
+          }
+          
+          var submitButton: some View {
+                    Button("Submit") {
+                              print("the user has submitted", $text)
+                              savePromp()
+                    }
+                    .frame(width: 120)
+                      .foregroundColor(.white)
+                      .background(Color(red: 0.0, green: 0.5882352941176471, blue: 0.7803921568627451))
+                      .cornerRadius(10)
+                      .padding(10)
+          }
+          
+          var submittedText: some View {
+                    Text(savedText)
+          }
+          
+          var submittedTextDescription: some View {
+                    Text(descriptionText)
+                              .font(.title3)
+                              .foregroundColor(Color(red: 0.6784313725490196, green: 0.9098039215686274, blue: 0.9568627450980393))
+                              .multilineTextAlignment(.leading)
+          }
+          
+          private func savePromp() {
+               savedText = text
+                    descriptionText = "You have entered following prompt: "
+           }
+}
+
