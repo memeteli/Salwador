eaa275acee2673461863b9276a3a550409eb1c56
maimaail <54360982+memeteli@users.noreply.github.com>
2023-02-07 15:56:31 +0100

    feat: implement http request

--- Salwador/ImageGenerator.swift
+++ Salwador/ImageGenerator.swift
@@ -14,56 +14,56 @@ class ImageGenerator {
     private init () {
         
     }
-    
-    func isPrompValid (_ prompt: String, apiKey: String) async throws -> Bool {
-        guard let url = URL(string: "https://api.openai.com/v1/moderations") else {
-            return false;
-        }
-       
-        let params:[String: Any] = [
-            "input" : prompt
-        ]
-        let data: Data = try JSONSerialization.data(withJSONObject: params)
-        
-        var request = URLRequest(url: url)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
-        request.httpMethod = "POST"
-        request.httpBody = data
-        
-        let (response, _) = try await URLSession.shared.data(for: request)
-        let result = try JSONDecoder().decode(ModerationResponse.self, from: response)
-        
-        return result.hasIssues == false
-    }
-    
+//
+//    func isPrompValid (_ prompt: String, apiKey: String) async throws -> Bool {
+//        guard let url = URL(string: "https://api.openai.com/v1/moderations") else {
+//            return false;
+//        }
+//
+//        let params:[String: Any] = [
+//            "input" : prompt
+//        ]
+//        let data: Data = try JSONSerialization.data(withJSONObject: params)
+//
+//        var request = URLRequest(url: url)
+//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+//        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+//        request.httpMethod = "POST"
+//        request.httpBody = data
+//
+//        let (response, _) = try await URLSession.shared.data(for: request)
+//        let result = try JSONDecoder().decode(ModerationResponse.self, from: response)
+//
+//        return result.hasIssues == false
+//    }
+
     func generateImage(withPrompt prompt: String, apiKey: String) async throws -> ImageGenerationResponse {
-        guard try await isPrompValid(prompt, apiKey: apiKey) else {
-            throw ImageError.inValidPrompt
-        }
+//        guard try await isPrompValid(prompt, apiKey: apiKey) else {
+//            throw ImageError.inValidPrompt
+//        }
         
         guard let url = URL(string: "https://api.openai.com/v1/images/generations") else {
             throw ImageError.badURL
         }
-        
+
         let params: [String: Any] = [
             "prompt" : prompt,
             "n" : 1,
-            "size" : "512x512",
+            "size" : "1024x1024",
             "user": sessionID
         ]
-        
-        let data = try JSONSerialization.data(withJSONObject: params)
-        
+
+        let data: Data = try JSONSerialization.data(withJSONObject: params)
+
         var request = URLRequest(url: url)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
         request.httpMethod = "POST"
         request.httpBody = data
-        
+
         let (response, _) = try await URLSession.shared.data(for: request)
         let result = try JSONDecoder().decode(ImageGenerationResponse.self, from: response)
-        
+
         return result
     }
 }
