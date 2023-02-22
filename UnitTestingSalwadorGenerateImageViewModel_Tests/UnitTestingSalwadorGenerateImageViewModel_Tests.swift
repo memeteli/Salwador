cf57bc992444a9c6e150ef3dff10b84dad136467
maimaail <54360982+memeteli@users.noreply.github.com>
2023-02-22 15:43:37 +0100

    feat: add new test target to GenerateImageViewModel

--- UnitTestingSalwadorGenerateImageViewModel_Tests/UnitTestingSalwadorGenerateImageViewModel_Tests.swift
+++ UnitTestingSalwadorGenerateImageViewModel_Tests/UnitTestingSalwadorGenerateImageViewModel_Tests.swift
@@ -1,13 +1,14 @@
 //
-//  UnitTestingSalwadorGenerateImageService_Tests.swift
-//  UnitTestingSalwadorGenerateImageService_Tests
+//  UnitTestingSalwadorGenerateImageViewModel_Tests.swift
+//  UnitTestingSalwadorGenerateImageViewModel_Tests
 //
 //  Created by Ailiniyazi Maimaiti on 22.02.23.
 //
 
 import XCTest
 
-final class UnitTestingSalwadorGenerateImageService_Tests: XCTestCase {
+final class UnitTestingSalwadorGenerateImageViewModel_Tests: XCTestCase {
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
