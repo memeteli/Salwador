873104832a2a16fdd653c693aa4d6e1c439493b3
maimaail <54360982+memeteli@users.noreply.github.com>
2023-02-22 14:58:23 +0100

    feat: add tests for get filepath func

--- UnitTestingSalwadorFilemanager_Tests/UnitTestingSalwadorFilemanager_Tests.swift
+++ UnitTestingSalwadorFilemanager_Tests/UnitTestingSalwadorFilemanager_Tests.swift
@@ -8,20 +8,29 @@
 import XCTest
 
 final class UnitTestingSalwadorFilemanager_Tests: XCTestCase {
+    //Configurations
+    private let fileName = "APIKey.json"
     private let fm = FileManagerService()
-    private let sampleFilePath = "/Users/uyghurbeg/Library/Developer/XCTestDevices/5D2C42E5-A987-46EC-990C-DA36DFF150EB/data/Containers/Bundle/Application/C8AC0B93-B121-4A31-9DBC-0560FFA546E1/Salwador.app/APIKey.json"
-
+    
+    let valid_filepath = FileManagerService().getFilePath(fileName: "APIKey", fileType: "json")
+    let not_valid_filepath = FileManagerService().getFilePath(fileName: "NoExistingJSONFile", fileType: "json")
+    
     override func setUpWithError() throws {}
-
+    
     override func tearDownWithError() throws {}
-
+    
     func test_UnitTestingSalwadorFilemanagerSevice_getFilePath_shouldReturnValidFilePath() {
-        let filepath = fm.getFilePath(fileName: "APIKey", fileType: "json")
-        XCTAssertEqual(filepath, sampleFilePath)
+        XCTAssertNotNil(valid_filepath)
+        XCTAssertNotEqual(valid_filepath, "")
+        XCTAssertTrue(valid_filepath.contains("Salwador.app/APIKey.json"))
     }
-
+    
     func test_UnitTestingSalwadorFileManagerService_getFilePath_shouldReturnEmptyString() {
-        let filepath = fm.getFilePath(fileName: "APIKey", fileType: "json")
-        XCTAssertNotEqual(filepath, sampleFilePath)
+        XCTAssertEqual(not_valid_filepath, "")
+        XCTAssertFalse(not_valid_filepath.contains(fileName))
+    }
+    
+    func test_UnitTestingSalwadorFileManagerService_getFileURL_shouldReturnValidFileURL() {
+        let
     }
 }
