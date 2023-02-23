d6ceaa27a35fd00f4cd75b76d8aeca10ce666a37
maimaail <54360982+memeteli@users.noreply.github.com>
2023-02-23 14:40:04 +0100

    feat: create generate random image feature

--- SalwadorApp.swift
+++ SalwadorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SalwadorApp: App {
     var body: some Scene {
         WindowGroup {
-            GenerateImageView()
+            GenerateSampleImageView()
         }
     }
 }
