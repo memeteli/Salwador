67d09a371d6759c92bb5c7648bdbee6146f402f1
maimaail <54360982+memeteli@users.noreply.github.com>
2023-02-27 08:59:21 +0100

    feat: add new views for navigation view

---
+++ Salwador/UIComponents/NavigationView/AppNavigationView.swift
@@ -0,0 +1,20 @@
+//
+//  AppNavigationView.swift
+//  Salwador
+//
+//  Created by Ailiniyazi Maimaiti on 24.02.23.
+//
+
+import SwiftUI
+
+struct AppNavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppNavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppNavigationView()
+    }
+}
