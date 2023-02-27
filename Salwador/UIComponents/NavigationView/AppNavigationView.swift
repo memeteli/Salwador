71203c8a23f915a56fe66837c56e676c4326d5ae
maimaail <54360982+memeteli@users.noreply.github.com>
2023-02-27 08:59:40 +0100

    feat: update navigation view

--- Salwador/UIComponents/NavigationView/AppNavigationView.swift
+++ Salwador/UIComponents/NavigationView/AppNavigationView.swift
@@ -9,12 +9,60 @@ import SwiftUI
 
 struct AppNavigationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                VStack {
+                    appNameView
+
+                    Spacer()
+
+                    NavigationLink(destination: GenerateImageView()) {
+                        Text("Create your own AI Image")
+                    }
+                    .frame(width: 275, height: 75)
+                    .foregroundColor(.white)
+                    .background(.pink)
+                    .cornerRadius(10)
+                    .padding(40)
+
+                    NavigationLink(destination: GenerateSampleImageView()) {
+                        Text("Get inspired from others")
+                    }
+                    .frame(width: 275, height: 75)
+                    .foregroundColor(.white)
+                    .background(.pink)
+                    .cornerRadius(10)
+
+                    Spacer()
+                }
+                .edgesIgnoringSafeArea(.all)
+                .background(Color("BackgroundColor"))
+            }
+        }
     }
 }
 
 struct AppNavigationView_Previews: PreviewProvider {
     static var previews: some View {
         AppNavigationView()
+    }
+}
+
+var appNameView: some View {
+    VStack {
+        Text("Salvador")
+            .foregroundColor(Color("TextColor"))
+            .font(.title)
+            .fontWeight(.bold)
+            .padding(.top, 30)
+
+        Image("salvador-icon")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 200, height: 100)
+
+        Text("Your AI Image Generator")
+            .foregroundColor(Color("TextColor"))
+            .font(.footnote)
     }
 }
